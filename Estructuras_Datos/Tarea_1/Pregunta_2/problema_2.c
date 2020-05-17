cbb22cf5fcc2b703355569ddeeafbd62b9f59219
root <root@DESKTOP-OF7LO8C.localdomain>
2020-05-17 00:09:47 -0400

    Problema 2 subido 1.0, me quiero cortar la pija xdxddddd

--- Estructuras_Datos/Tarea_1/Pregunta_2/problema_2.c
+++ Estructuras_Datos/Tarea_1/Pregunta_2/problema_2.c
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
 }
 
 void actualizarSaldos (char* clientes, char* transacciones) {
-    FILE* archivo_clientes, archivo_transacciones;
+    FILE* archivo_clientes, *archivo_transacciones;
     if (archivo_clientes = fopen(clientes, "r+")) {
         printf("Hubo un error al abrir el archivo %s.", clientes);
         exit(1);
@@ -30,8 +30,57 @@ void actualizarSaldos (char* clientes, char* transacciones) {
         printf("Hubo un error al abrir el archivo %s.", transacciones);
         exit(1);
     }
-    
 
+    clienteBanco cuenta;
 
-    
+    int contador = 0;
+    char temp;
+    while (fread(&cuenta, sizeof(clienteBanco), 1, archivo_clientes) != 0) {
+
+        char tipo_transaccion = getc(archivo_transacciones);
+        int cuenta_1, cuenta_2, cantidad_dinero, bandera = 1, dinero_temporal = cuenta.saldo;
+
+        while (bandera) {
+            if (tipo_transaccion == '+') {
+                fscanf(archivo_transacciones, " %d %d", &cuenta_1, &cantidad_dinero);
+                if (cuenta_1 == cuenta.nroCuenta) {
+                    cuenta.saldo += cantidad_dinero;
+                }
+            }
+            else if (tipo_transaccion == '-') {
+                fscanf(archivo_transacciones, " %d %d", &cuenta_1, &cantidad_dinero);
+                if (cuenta_1 == cuenta.nroCuenta) {
+                    cuenta.saldo -= cantidad_dinero;
+                }
+            }
+            else if (tipo_transaccion == '>') {
+                fscanf(archivo_transacciones, " %d %d %d", &cuenta_1, &cuenta_2, &cantidad_dinero);
+                if (cuenta_1 == cuenta.nroCuenta) {
+                    cuenta.saldo -= cantidad_dinero;
+                } 
+                else if (cuenta_2 == cuenta.nroCuenta) {
+                    cuenta.saldo += cantidad_dinero;
+                }
+            }
+
+            while ((tipo_transaccion = getc(archivo_transacciones)) != '\n' && tipo_transaccion != EOF)
+                ;
+            if (tipo_transaccion == EOF)
+            {
+                bandera = 0;
+            }
+            else
+            {
+                tipo_transaccion = getc(archivo_transacciones);
+            }
+        }
+
+        fseek(archivo_clientes, contador, SEEK_SET);
+        fwrite(&cuenta, sizeof(clienteBanco), 1, archivo_clientes);
+
+        contador += sizeof(clienteBanco);
+    }
+
+    fclose(archivo_clientes);
+    fclose(archivo_transacciones);
 }
